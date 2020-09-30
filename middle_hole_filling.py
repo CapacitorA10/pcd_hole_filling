8f341669102abdabbe334fc66d829ff1fd4286fd
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2020-09-30 14:54:51 +0900

    구멍 채우기 알고리즘을 center_vertex_making으로 분리

--- middle_hole_filling.py
+++ middle_hole_filling.py
@@ -4,17 +4,19 @@ from hole_detection import hole_detection
 from edge_swapping import edge_swapping
 from center_vertex_making import center_vertex_making
 
+#mesh load
 mesh = o3d.io.read_triangle_mesh("D:/pointcloud\\sphere_hole2.ply")
 mesh_old = o3d.io.read_triangle_mesh("D:/pointcloud\\sphere_hole2.ply")
+
+#hole detect
 boundary_triangle, boundary_line_i, boundary_vertex_i = hole_detection(mesh)
-#o3d.visualization.draw_geometries([mesh], mesh_show_wireframe=True) # 출력
 
+#hole filling
 mesh, added_num = center_vertex_making(mesh)
-
 o3d.visualization.draw_geometries([mesh], mesh_show_wireframe=True) # 출력
 
 
-"""Edge Swapping"""
+# Edge swapping
 mesh = edge_swapping(mesh, mesh_old, added_num,num=1)
 mesh = edge_swapping(mesh, mesh_old, added_num,num=2)
 
